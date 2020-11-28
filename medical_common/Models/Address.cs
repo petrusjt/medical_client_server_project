d849fa27c7d710459c827ac4de52d76bcd508759
petrusjt <jozsefpetrus99@gmial.com>
2020-11-28 11:58:09 +0100

    closes #3 ; Added Model for the Patient

---
+++ medical_common/Models/Address.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace medical_common.Models
+{
+    public class Address
+    {
+        private string _country { get; set; }
+        private string _region { get; set; }
+        private string _city { get; set; }
+        private string _streetName { get; set; }
+        private int _streetNumber { get; set; }
+        private char _staircaseRef { get; set; }
+        private int _floor { get; set; }
+        private int _apartmentNumber { get; set; }
+
+        public Address(string country, string region, string city, string street_name, int street_number, char staircase_ref=' ', int floor=0, int apartment_number=0)
+        {
+            _country = country;
+            _region = region;
+            _city = city;
+            _streetName = street_name;
+            _streetNumber = street_number;
+            _staircaseRef = staircase_ref;
+            _floor = floor;
+            _apartmentNumber = apartment_number;
+        }
+    }
+}
---
+++ medical_common/Models/Patient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace medical_common.Models
+{
+    public class Patient
+    {
+        private string _name { get; set; }
+        private Address _address { get; set; }
+        // TAJ: Hungarian social security number
+        private string _TAJ { get; set; }
+        private string _problem { get; set; }
+        private DateTime _timeRegistered { get; set; }
+
+        public Patient(string name, Address address, string tAJ, string problem)
+        {
+            _name = name;
+            _address = address;
+            _TAJ = tAJ;
+            _problem = problem;
+            _timeRegistered = DateTime.Now;
+        }
+
+        public Patient(string name, Address address, string tAJ, string problem, DateTime timeRegistered) : this(name, address, tAJ, problem)
+        {
+            _timeRegistered = timeRegistered;
+        }
+    }
+}
