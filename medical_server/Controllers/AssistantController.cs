877451085287de4392a17a27e013ebd61f80b95e
petrusjt <jozsefpetrus99@gmial.com>
2020-11-28 21:24:52 +0100

    closes #5 ; Added Controller for Assistant-side client

---
+++ medical_server/Controllers/AssistantController.cs
@@ -0,0 +1,45 @@
+﻿using medical_common.Models;
+using medical_server.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace medical_server.Controllers
+{
+	[Route("api/assistant")]
+	[ApiController]
+	public class AssistantController : ControllerBase
+	{
+		[HttpGet]
+		public ActionResult<string> HelloWorld()
+		{
+			return Ok("Hello World!");
+		}
+		[Route("getallpatients")]
+		[HttpGet]
+		public ActionResult<List<Patient>> GetAllPatients()
+        {
+			return Ok(PatientRepository.LoadPatients());
+        }
+
+		[Route("addpatient")]
+		[HttpPost]
+		public ActionResult AddPatient([FromBody]Patient patient)
+        {
+			var patients = PatientRepository.LoadPatients();
+			if(patients.Contains(patient))
+            {
+				return Conflict();
+            }
+			else
+            {
+				patients.Add(patient);
+				PatientRepository.WritePatients(patients);
+				return Ok();
+			}
+        }
+	}
+}
