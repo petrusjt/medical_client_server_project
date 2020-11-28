7508add68a2d3c65fec60d61e46c39308fe1e22d
petrusjt <jozsefpetrus99@gmial.com>
2020-11-28 21:21:42 +0100

    Added Equals and GetHashCode methods

--- medical_common/Models/Patient.cs
+++ medical_common/Models/Patient.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace medical_common.Models
 {
-    public class Patient
+    public class Patient : IEquatable<Patient>
     {
         public string Name { get; set; }
         public Address Address { get; set; }
@@ -36,6 +36,30 @@ namespace medical_common.Models
         public override string ToString()
         {
             return $"Patient({Name}, {Address}, {TAJ}, {Problem}, {TimeRegistered})";
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Patient);
+        }
+
+        public bool Equals(Patient other)
+        {
+            return other != null &&
+                   Name == other.Name &&
+                   EqualityComparer<Address>.Default.Equals(Address, other.Address) &&
+                   TAJ == other.TAJ &&
+                   Problem == other.Problem;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = 931181849;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Name);
+            hashCode = hashCode * -1521134295 + EqualityComparer<Address>.Default.GetHashCode(Address);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(TAJ);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Problem);
+            return hashCode;
         }
     }
 }
