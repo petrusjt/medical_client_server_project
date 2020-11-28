5580c09814f943ba49557de2ffd6b29dfc0f2eb0
petrusjt <jozsefpetrus99@gmial.com>
2020-11-28 17:49:26 +0100

    closes #2 ; Created PatientRepository

---
+++ medical_server/Repositories/PatientRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using medical_common.Models;
+using System.IO;
+
+namespace medical_server.Repositories
+{
+    public static class PatientRepository
+    {
+        static string appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        static string filePath = appDataPath + "\\" + "PatientData\\data.json";
+
+        public static void WritePatients(IList<Patient> patients)
+        {
+            if(!Directory.Exists(appDataPath + "\\" + "PatientData"))
+            {
+                Directory.CreateDirectory(appDataPath + "\\" + "PatientData");
+            }
+            string jsonString = JsonSerializer.Serialize(patients);
+            File.WriteAllText(filePath, jsonString);
+        }
+
+        public static IList<Patient> LoadPatients()
+        {
+            if (Directory.Exists(appDataPath + "\\" + "PatientData"))
+            {
+                if (File.Exists(filePath))
+                {
+                    string json = File.ReadAllText(filePath);
+                    List<Patient> patients = JsonSerializer.Deserialize<List<Patient>>(json);
+                    return patients;
+                }
+            }
+            return new List<Patient>();
+        }
+    }
+}
