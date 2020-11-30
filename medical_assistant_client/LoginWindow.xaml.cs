f269d9a4c6b44ba3cd4e06019acb90882de00768
petrusjt <jozsefpetrus99@gmial.com>
2020-11-30 20:06:31 +0100

    closes #6 ; Added GUI for the Assistant Client

---
+++ medical_assistant_client/LoginWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xaml;
+
+namespace medical_assistant_client
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            PatientRegistrationWindow patientRegistration = new PatientRegistrationWindow();
+            patientRegistration.Show();
+            patientRegistration.Closing += new CancelEventHandler((object sender2, CancelEventArgs e2) => this.Show());
+            this.Hide();
+            
+        }
+    }
+}
