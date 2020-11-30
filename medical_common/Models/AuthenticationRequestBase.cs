7de18657bd2a228562200cacf3d5f535385dad2a
petrusjt <jozsefpetrus99@gmial.com>
2020-11-30 16:28:57 +0100

    Common: Added AuthenticationRequestBase.cs

---
+++ medical_common/Models/AuthenticationRequestBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace medical_common.Models
+{
+    public class AuthenticationRequestBase
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
