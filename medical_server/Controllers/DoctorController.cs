a47f218b16dba3f5bd3527eebf5589fcddbf499e
Savitar97 <atoth1571@gmail.com>
2020-12-25 19:29:25 +0100

    closes #17; Create update and delete patient[DoctorController]

--- medical_server/Controllers/DoctorController.cs
+++ medical_server/Controllers/DoctorController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using medical_common.Models;
 using medical_server.Authentication;
+using medical_server.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,13 +20,50 @@ namespace medical_server.Controllers
             return Ok("Hello World!");
         }
 
-        
+
         [Route("authdoctor")]
         [HttpPost]
         public ActionResult AuthenticateDoctor([FromBody] AuthenticationRequestBase authenticationRequest)
         {
             DoctorAuthenticator doctorAuthenticator = new DoctorAuthenticator();
             return doctorAuthenticator.Authenticate(authenticationRequest.Username, authenticationRequest.Password);
+        }
+
+
+        [HttpPut]
+        public ActionResult UpdatePatient(Patient patient)
+        {
+            var patients = PatientRepository.LoadPatients();
+
+            var oldPatient = patients.FirstOrDefault(x => x.TAJ == patient.TAJ);
+            if (oldPatient != null)
+            {
+                oldPatient.Name = patient.Name;
+                oldPatient.TAJ = patient.TAJ;
+                oldPatient.Problem = patient.Problem;
+                oldPatient.Address = patient.Address;
+                oldPatient.Diagnosis = patient.Diagnosis;
+            }
+            else
+            {
+                patients.Add(patient);
+            }
+            PatientRepository.WritePatients(patients);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeletePatient(string id)
+        {
+            var patients = PatientRepository.LoadPatients();
+            var patient = patients.FirstOrDefault(x => x.TAJ == id);
+            if (patient != null)
+            {
+                patients.Remove(patient);
+                PatientRepository.WritePatients(patients);
+                return Ok();
+            }
+            return NotFound();
         }
 
     }
